a9dd88d3eec8a3e4e78cdc335fb16966d833ac9f
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-08-29 11:52:11 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form');
+  let ul = document.createElement('ul');
+  document.getElementById('list').append(ul);
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const value = document.querySelector('#new-task-description').value
+    let li = document.createElement('li');
+    li.innerText=value
+    ul.append(li);
+  })
 });
+
+
